c977065ed149dd48f843c469ba09a25f996e4a5e
Tao Xiang <46348901+leoxiang66@users.noreply.github.com>
2024-05-29 18:29:07 +0800

    1

---
+++ reflexapp/state.py
@@ -0,0 +1,101 @@
+import os
+import reflex as rx
+
+
+
+
+
+class QA(rx.Base):
+    """A question and answer pair."""
+
+    question: str
+    answer: str
+
+
+DEFAULT_CHATS = {
+    "Intros": [],
+}
+
+
+class State(rx.State):
+    """The app state."""
+
+    # A dict from the chat name to the list of questions and answers.
+    chats: dict[str, list[QA]] = DEFAULT_CHATS
+
+    # The current chat name.
+    current_chat = "Intros"
+
+    # The current question.
+    question: str
+
+    # Whether we are processing the question.
+    processing: bool = False
+
+    # The name of the new chat.
+    new_chat_name: str = ""
+
+    def create_chat(self):
+        """Create a new chat."""
+        # Add the new chat to the list of chats.
+        self.current_chat = self.new_chat_name
+        self.chats[self.new_chat_name] = []
+
+    def delete_chat(self):
+        """Delete the current chat."""
+        del self.chats[self.current_chat]
+        if len(self.chats) == 0:
+            self.chats = DEFAULT_CHATS
+        self.current_chat = list(self.chats.keys())[0]
+
+    def set_chat(self, chat_name: str):
+        """Set the name of the current chat.
+
+        Args:
+            chat_name: The name of the chat.
+        """
+        self.current_chat = chat_name
+
+    @rx.var
+    def chat_titles(self) -> list[str]:
+        """Get the list of chat titles.
+
+        Returns:
+            The list of chat names.
+        """
+        return list(self.chats.keys())
+
+    async def process_question(self, form_data: dict[str, str]):
+        # Get the question from the form
+        question = form_data["question"]
+
+        # Check if the question is empty
+        if question == "":
+            return
+
+        model = self.openai_process_question
+
+        async for value in model(question):
+            yield value
+
+    async def openai_process_question(self, question: str):
+        """Directly return user input as the response.
+        
+        Args:
+            question: The user's question.
+        """
+
+        # Add the question to the list of questions.
+        qa = QA(question=question, answer=question)  # Here we set answer directly as the input question
+        self.chats[self.current_chat].append(qa)
+
+        # Clear the input and start the processing.
+        self.processing = True
+        yield
+
+        # Toggle the processing flag off as processing is instant.
+        self.processing = False
+
+        # Since the process is synchronous, notify the change immediately.
+        self.chats = self.chats
+        yield
